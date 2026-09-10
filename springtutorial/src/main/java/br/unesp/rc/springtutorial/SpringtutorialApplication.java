d6507105c348ddf9384af635cff9ef6ae1e583e7
Mateus Segalla Franco <mateus.segalla@gmail.com>
2026-09-09 21:44:02 -0300

    Parte 2: InstanceGenerator e teste de persistencia funcionando

--- springtutorial/src/main/java/br/unesp/rc/springtutorial/SpringtutorialApplication.java
+++ springtutorial/src/main/java/br/unesp/rc/springtutorial/SpringtutorialApplication.java
@@ -1,13 +1,24 @@
 package br.unesp.rc.springtutorial;
 
+import br.unesp.rc.springtutorial.entity.Fisica;
+import br.unesp.rc.springtutorial.utils.InstanceGenerator;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Prof. Dr. Frank J. Affonso
+ */
 @SpringBootApplication
-public class SpringtutorialApplication {
+public class SpringtutorialApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringtutorialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtutorialApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Fisica entity = InstanceGenerator.getPessoaFisica("111.222.333-44", "user1");
+        System.out.println("\n" + entity + "\n");
+    }
 }
