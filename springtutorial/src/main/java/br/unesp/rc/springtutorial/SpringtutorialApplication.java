4060ddd363dd9719a27fa1fa123e1796007dc798
Mateus Segalla Franco <mateus.segalla@gmail.com>
2026-09-09 21:48:01 -0300

    Parte 2: camada service criada, persistencia no MySQL confirmada

--- springtutorial/src/main/java/br/unesp/rc/springtutorial/SpringtutorialApplication.java
+++ springtutorial/src/main/java/br/unesp/rc/springtutorial/SpringtutorialApplication.java
@@ -1,7 +1,9 @@
 package br.unesp.rc.springtutorial;
 
 import br.unesp.rc.springtutorial.entity.Fisica;
+import br.unesp.rc.springtutorial.service.FisicaService;
 import br.unesp.rc.springtutorial.utils.InstanceGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +14,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringtutorialApplication implements CommandLineRunner {
 
+    @Autowired
+    private FisicaService fs;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringtutorialApplication.class, args);
     }
@@ -19,6 +24,7 @@ public class SpringtutorialApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Fisica entity = InstanceGenerator.getPessoaFisica("111.222.333-44", "user1");
-        System.out.println("\n" + entity + "\n");
+        Fisica saved = fs.save(entity);
+        System.out.println("\nPessoa salva no banco:\n" + saved + "\n");
     }
 }
